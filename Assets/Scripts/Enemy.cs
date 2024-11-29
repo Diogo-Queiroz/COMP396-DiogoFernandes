0e6c5995ff8006a205115d910003da56c3ffac36
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2024-11-28 21:17:16 -0500

    Initial Builder Pattern system for Enemy. Need to include Enemy Physical Body and Animation.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,13 +12,23 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject[] _patrolPoints;
 
+    public string Name;
+    public float Speed;
+    public int Damage;
+    public int Health;
+    public string AnimatonControllerPath;
+    public GameObject Body;
+
     private StateMachine _stateMachine;
 
     private void Awake()
     {
         _player = GameObject.FindWithTag("Player");
         _animator = GetComponent<Animator>();
+        Debug.Log($"Does animpath exists? {AnimatonControllerPath}");
+        _animator.runtimeAnimatorController = Resources.Load(AnimatonControllerPath) as RuntimeAnimatorController;
         _agent = GetComponent<NavMeshAgent>();
+        _patrolPoints = GameObject.FindGameObjectsWithTag("PatrolLocations");
     }
 
     private void Start()
@@ -43,5 +53,57 @@ public class Enemy : MonoBehaviour
     private void FixedUpdate()
     {
         _stateMachine.FixedUpdate();
+    }
+
+    public class Builder
+    {
+        private string name = "Enemy";
+        private float speed = 5f;
+        private int damage = 10;
+        private int health = 100;
+        private string animatonControllerPath = string.Empty;
+        private GameObject body;
+
+        public Builder WithAnimation(string animatonControllerPath)
+        {
+            this.animatonControllerPath = animatonControllerPath;
+            return this;
+        }
+        public Builder WithBody(GameObject body)
+        {
+            this.body = body;
+            return this;
+        }
+        public Builder WithName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+        public Builder WithSpeed(float speed)
+        {
+            this.speed = speed;
+            return this;
+        }
+        public Builder WithDamage(int damage)
+        {
+            this.damage = damage;
+            return this;
+        }
+        public Builder WithHealth(int health)
+        {
+            this.health = health;
+            return this;
+        }
+        public Enemy Build()
+        {
+            Enemy enemy = new GameObject(name).AddComponent<Enemy>();
+            enemy.Name = name;
+            enemy.Speed = speed;
+            enemy.Damage = damage;
+            enemy.Health = health;
+            enemy.AnimatonControllerPath = animatonControllerPath;
+            enemy.Body = body;
+            return enemy;
+        }
     }
 }
---
+++ Assets/Scripts/EnemyCommonData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/Create Enemy", fileName = "Enemy Data")]
+public class EnemyCommonData : ScriptableObject
+{
+    public string animControllerPath;
+    public GameObject body;
+}
