d3106b3bd9c78b387eed2aaa3182cc0ffe4e442d
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2024-12-02 20:03:21 -0500

    Week 13 - Enemy Builder complete. Before GOAP from Unity CrashKonijn package installation.

--- Assets/Scripts/EnemyCommonData.cs
+++ Assets/Scripts/EnemyCommonData.cs
@@ -5,4 +5,25 @@ public class EnemyCommonData : ScriptableObject
 {
     public string animControllerPath;
     public GameObject body;
+    public AudioSourceSettings audioSourceSettings;
+    public ColliderSettings colliderSettings;
+}
+
+[System.Serializable]
+public struct AudioSourceSettings
+{
+    public bool AudioSourcePlayOnAwake;
+    public float AudioSourceSpatialBlend; // Change to 3d if 1 or 2d if 0
+    public float AudioSourceMinDistance; // Min Distance to be heard if 3D
+    public float AudioSourceMaxDistance; // Max Distance to be head if 3D
+    public AudioRolloffMode AudioRolloffMode; // Change the mode to either Logarithmic or Linear
+    public AudioClip audioClip;
+}
+
+[System.Serializable] 
+public struct ColliderSettings
+{
+    public float ColliderCenter;
+    public float ColliderRadius;
+    public float ColliderHeight;
 }
